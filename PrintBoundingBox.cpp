8c2f3bcbd8a129ed1b923ea49bf07f82da5f7011
SherAndrei <welldryus@gmail.com>
2023-11-30 14:07:43 +0300

    PrintBoundingBox: print bounding box of the largest contour

--- PrintBoundingBox.cpp
+++ PrintBoundingBox.cpp
@@ -7,6 +7,7 @@
 using namespace cv;
 using namespace std;
 
+Mat src;
 Mat src_gray;
 int thresh = 100;
 RNG rng(12345);
@@ -14,7 +15,7 @@ void thresh_callback(int, void*);
 
 int main(int argc, char** argv) {
   CommandLineParser parser(argc, argv, "{@input | stuff.jpg | input image}");
-  Mat src = imread(samples::findFile(parser.get<String>("@input")));
+  src = imread(samples::findFile(parser.get<String>("@input")));
   if (src.empty()) {
     cout << "Could not open or find the image!\n" << endl;
     cout << "usage: " << argv[0] << " <Input image>" << endl;
@@ -24,7 +25,6 @@ int main(int argc, char** argv) {
   blur(src_gray, src_gray, Size(3, 3));
   const char* source_window = "Source";
   namedWindow(source_window);
-  imshow(source_window, src);
   const int max_thresh = 255;
   createTrackbar("Canny thresh:", source_window, &thresh, max_thresh,
                  thresh_callback);
@@ -32,27 +32,28 @@ int main(int argc, char** argv) {
   waitKey();
   return 0;
 }
+
 void thresh_callback(int, void*) {
   Mat canny_output;
   Canny(src_gray, canny_output, thresh, thresh * 2);
   vector<vector<Point>> contours;
-  findContours(canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
-  vector<vector<Point>> contours_poly(contours.size());
-  vector<Rect> boundRect(contours.size());
-  vector<Point2f> centers(contours.size());
-  vector<float> radius(contours.size());
-  for (size_t i = 0; i < contours.size(); i++) {
-    approxPolyDP(contours[i], contours_poly[i], 3, true);
-    boundRect[i] = boundingRect(contours_poly[i]);
-    minEnclosingCircle(contours_poly[i], centers[i], radius[i]);
-  }
+  findContours(canny_output, contours, RETR_LIST, CHAIN_APPROX_NONE);
+  const auto by_size = [](const auto& l, const auto& r) { return l.size() < r.size(); };
+  const auto largest_contour = std::max_element(contours.begin(), contours.end(), by_size);
+
   Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-  for (size_t i = 0; i < contours.size(); i++) {
-    Scalar color =
-        Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-    drawContours(drawing, contours_poly, (int)i, color);
-    rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2);
-    circle(drawing, centers[i], (int)radius[i], color, 2);
-  }
-  imshow("Contours", drawing);
+
+  const auto thickness = 3;
+
+  const auto bounding_box = boundingRect(*largest_contour);
+  const auto red = Scalar(0, 0, 255);
+  rectangle(drawing, bounding_box, red, thickness);
+
+  const auto color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+  const auto draw_all_contours = -1;
+  vector<Point> contour_poly;
+  approxPolyDP(*largest_contour, contour_poly, 3, false);
+  drawContours(drawing, std::vector<decltype(contour_poly)>{ std::move(contour_poly) }, draw_all_contours, color, thickness);
+
+  imshow("Source", src + drawing);
 }
